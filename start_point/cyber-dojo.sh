bdac1da0b2e57ae369618ff78bde32ee9b20c83a
JonJagger <jon@kosli.com>
2025-10-09 20:33:25 +0100

    Tidy layout of call to pycodestyle

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -48,13 +48,12 @@ coverage3 report \
 
 # http://pycodestyle.pycqa.org/en/latest/intro.html#configuration
 
-pycodestyle \
-  ${CYBER_DOJO_SANDBOX} \
-    --show-source `# show source code for each error` \
-    --show-pep8   `# show relevent text from pep8` \
-    --ignore E302,E305,W293 \
-    --max-line-length=80 \
-      > ${REPORT_DIR}/style.txt
+pycodestyle ${CYBER_DOJO_SANDBOX} \
+  --show-source `# show source code for each error` \
+  --show-pep8   `# show relevent text from pep8` \
+  --ignore E302,E305,W293 \
+  --max-line-length=80 \
+  > ${REPORT_DIR}/style.txt
 
 # E302 expected 2 blank lines, found 0
 # E305 expected 2 blank lines after end of function or class
